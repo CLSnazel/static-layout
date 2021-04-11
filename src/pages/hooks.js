51026b06c1be3ab36c4a9ef019ef658b967be1e6
Chantal Snazel <clsnazel@gmail.com>
2021-04-10 20:53:43 -0700

    style: move hook components to components/hook-components

--- src/pages/hooks.js
+++ src/pages/hooks.js
@@ -1,10 +1,10 @@
 import React from 'react'
-import BreakpointNumbers from '../components/BreakpointNumbers'
-import CurrentBreakpointHeading from '../components/CurrentBreakpointHeading'
-import MediaQueryParagraph from '../components/MediaQueryParagraph'
-import PropControlledParagraph from '../components/PropControlledParagraph'
-import ResponsiveQueryParagraph from '../components/ResponsiveQueryParagraph'
-import ScreenSizeHeading from '../components/ScreenSizeHeading'
+import BreakpointNumbers from '../components/hook-components/BreakpointNumbers'
+import CurrentBreakpointHeading from '../components/hook-components/CurrentBreakpointHeading'
+import MediaQueryParagraph from '../components/hook-components/MediaQueryParagraph'
+import PropControlledParagraph from '../components/hook-components/PropControlledParagraph'
+import ResponsiveQueryParagraph from '../components/hook-components/ResponsiveQueryParagraph'
+import ScreenSizeHeading from '../components/hook-components/ScreenSizeHeading'
 
 export default function Hooks () {
   return (
