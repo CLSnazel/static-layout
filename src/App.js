0f91048977f271852979b20d7b292fe49e7ae10f
Chantal Snazel <clsnazel@gmail.com>
2021-04-10 19:41:21 -0700

    feat: add custom breakpoints to top-level app

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, { useEffect } from 'react'
 import { Root, Routes, addPrefetchExcludes } from 'react-static'
 //
 import { Link, Router } from 'components/Router'
@@ -6,23 +6,44 @@ import Dynamic from 'containers/Dynamic'
 
 import './app.css'
 
+import Layout from 'atomic-layout'
 // Any routes that start with 'dynamic' will be treated as non-static routes
 addPrefetchExcludes(['dynamic'])
 
-function App() {
+const breakpoints = {
+  small: {
+    minWidth: 0,
+    maxWidth: 767
+  },
+  medium: {
+    minWidth: 768,
+    maxWidth: 1339
+  },
+  large: {
+    minWidth: 1440
+  }
+}
+
+function App () {
+  useEffect(() => {
+    Layout.configure({
+      breakpoints,
+      defaultBreakpointName: 'small'
+    })
+  })
   return (
     <Root>
       <nav>
-        <Link to="/">Home</Link>
-        <Link to="/about">About</Link>
-        <Link to="/blog">Blog</Link>
-        <Link to="/dynamic">Dynamic</Link>
+        <Link to='/'>Home</Link>
+        <Link to='/about'>About</Link>
+        <Link to='/blog'>Blog</Link>
+        <Link to='/dynamic'>Dynamic</Link>
       </nav>
-      <div className="content">
+      <div className='content'>
         <React.Suspense fallback={<em>Loading...</em>}>
           <Router>
-            <Dynamic path="dynamic" />
-            <Routes path="*" />
+            <Dynamic path='dynamic' />
+            <Routes path='*' />
           </Router>
         </React.Suspense>
       </div>
